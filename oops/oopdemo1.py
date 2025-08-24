33264de3aee7c2674a1ff3fe40c38e85b58b9731
Rahul Dharpure <Rahuldharpure2000@gmail.com>
2025-08-24 12:48:18 +0530

    final commit

--- oops/oopdemo1.py
+++ oops/oopdemo1.py
@@ -4,20 +4,20 @@
 #  1. Instance Variable : Created per instance basis
 #  2. Local Variables : Created locally inside a method and destroy when
 #                       the method execution is over.
-#  3. Class Variables : 
-
+#  3. Class Variables :
 
 
 class Emp:
+    # constructor
     def __init__(self, name, age, salary):
         self.name = name
         self.age = age
         self.salary = salary
 
+    # instance method
     def show(self):
         print(f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}")
 
 
 e = Emp("Rahul", 25, 30000.0)
 e.show()
-
--- oops/oopdemo2.py
+++ oops/oopdemo2.py
@@ -1,13 +1,27 @@
 import math
 
 
+# Types of methods
+# 1. Instance method : called using object reference
+# 2. Class method : called using class name
+# 3. Static method : called using class name
+
+# Instance method : 
+
+# 1. These are called instance methods because they can access instance members of the object.
+# 2. The first argument is always self which refers to the current object.
+# 3. We can access instance variables and instance methods using self keyword.
+
 class Circle:
+    # constructor
     def __init__(self, radius):
         self.radius = radius
+    # instance method
 
     def cal_area(self):
         area = math.pi * self.radius * self.radius
         print("Area:", area)
+    # instance method
 
     def cal_circumference(self):
         circ = 2 * math.pi * self.radius
