27fd1b6c5da2653dd7532ba39d2281aaa2598221
Rahul Dharpure <Rahuldharpure2000@gmail.com>
2025-08-25 21:57:04 +0530

    str demo10

--- oops/strdemo10.py
+++ oops/strdemo10.py
@@ -1,4 +1,16 @@
-# __str__ method : to return a string representation of the object
+# Demonstration of the __str__ method: returns a string representation of an object.
+
+# Key Points:
+# 1. Since Python 3.0, every user-defined class automatically inherits from the base 'object' class.
+#    (Explicitly: class Emp(object): or implicitly: class Emp:)
+
+# 2. The 'object' class provides several special (magic) methods that all classes inherit.
+
+# 3. Common special methods include:
+#    - __init__(): Constructor, called when an object is created.
+#    - __str__(): Returns a string representation of the object (used by print()).
+#    - __new__(): Creates a new instance of the class.
+#    - __del__(): Called when an object is about to be destroyed.
 
 
 class Emp:
