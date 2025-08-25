860fa0d05e96a3df76b5f287ddb53b9761a25852
Rahul Dharpure <Rahuldharpure2000@gmail.com>
2025-08-25 15:06:49 +0530

    static method demo9

--- oops/staticmethoddemo9.py
+++ oops/staticmethoddemo9.py
@@ -1,7 +1,19 @@
+# Static method : A method which does not take self or cls as the first argument
+
+
+#   If we do not use the decorator @staticmethod, then 2 things can happen:
+
+#   1. If we call the method using the object reference, Python will consider it to
+#      be an instance method
+#   2. If we are calling it using the classname, then Python will consider it to be a
+#      static method
+
+
 class MyMath:
     @staticmethod
     def add(x, y):
         return x + y
+
 
 result = MyMath.add(5, 3)
 print("The sum is:", result)
