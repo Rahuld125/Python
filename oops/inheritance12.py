384a6685f0a3cea31fdd010a46a30ef6e89e6314
Rahul Dharpure <Rahuldharpure2000@gmail.com>
2025-08-26 21:13:14 +0530

    inheritance 12

--- oops/inheritance12.py
+++ oops/inheritance12.py
@@ -5,19 +5,21 @@
 class Animal:
     def eat(self):
         print("Animal eats")
-    
+
     def sleep(self):
         print("Animal sleeps")
+
 
 class Bird(Animal):
     def set_type(self, type):
         self.type = type
-    
+
     def fly(self):
         print("It flies in the sky")
 
     def __str__(self):
         return "This is a " + self.type
+
 
 duck = Bird()
 duck.set_type("Duck")
