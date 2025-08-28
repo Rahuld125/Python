0cc551101d323722d7d3cbd459f645d380779b2d
Rahul Dharpure <Rahuldharpure2000@gmail.com>
2025-08-29 00:02:25 +0530

    exception 21

---
+++ exceptions/finallydemo22.py
@@ -0,0 +1,13 @@
+import sys
+
+
+try:
+    a = int(input("Enter first no:"))
+    b = int(input("Enter second no:"))
+    c = a / b
+    print("Div is ", c)
+    sys.exit(0)
+except ZeroDivisionError:
+    print("Denominator must not be 0")
+finally:
+    print("Have a good day!")
