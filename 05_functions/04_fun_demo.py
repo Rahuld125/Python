7d601d5609fd75313814de2eeaea51934ce1b687
Rahul Dharpure <69070030+Rahuld125@users.noreply.github.com>
2025-09-03 23:10:13 +0530

    function

---
+++ 05_functions/04_fun_demo.py
@@ -0,0 +1,8 @@
+def grocery(name, price):
+    print("Item is",name,"Its price is",price)
+
+grocery(name="Bread", price=20)
+grocery(price=500, name="Butter")
+
+# positional args comes first, then keyword args
+# default args
